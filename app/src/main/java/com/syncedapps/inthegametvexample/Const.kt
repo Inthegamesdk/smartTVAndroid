a40f8b87843b733de04f12c7ea60cffc9e165efa
romanhavdulskyi <romeogavdulskiy@gmail.com>
2023-03-17 14:51:59 +0200

    Set kiddodle test channel #2

--- app/src/main/java/com/syncedapps/inthegametvexample/Const.kt
+++ app/src/main/java/com/syncedapps/inthegametvexample/Const.kt
@@ -4,7 +4,7 @@ import com.syncedapps.inthegametv.network.ITGEnvironment
 
 object Const {
     const val ACCOUNT_ID = "637a54561ffe416ac9027a53"
-    const val CHANNEL_SLUG = "testChannel2"
+    const val CHANNEL_SLUG = "89473"
     const val LANGUAGE = "en"
     val environment = ITGEnvironment.v2_0
 }
