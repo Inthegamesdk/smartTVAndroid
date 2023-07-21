691dee6f7b34c4a4a73519012deba5e09b656404
romanhavdulskyi <romeogavdulskiy@gmail.com>
2023-07-21 19:32:49 +0300

    Updated creds

--- app/src/main/java/com/syncedapps/inthegametvexample/Const.kt
+++ app/src/main/java/com/syncedapps/inthegametvexample/Const.kt
@@ -3,8 +3,8 @@ package com.syncedapps.inthegametvexample
 import com.syncedapps.inthegametv.network.ITGEnvironment
 
 object Const {
-    const val ACCOUNT_ID = "637a54561ffe416ac9027a53"
-    const val CHANNEL_SLUG = "89473"
+    const val ACCOUNT_ID = "64100c941dca8096fc0df832"
+    const val CHANNEL_SLUG = "dudeplusdemo1"
     const val LANGUAGE = "en"
-    val environment = ITGEnvironment.v2_0
+    val environment = ITGEnvironment.v2_1
 }
